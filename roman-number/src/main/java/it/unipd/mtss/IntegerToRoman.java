d67d7ae2975f2bafe5feac5ead08c47bf58d8e87
agnese-carraro <agnese.carraro@studenti.unipd.it>
2024-05-18 16:31:53 +0200

    inizializzato maven

---
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -0,0 +1,13 @@
+////////////////////////////////////////////////////////////////////
+// [Agnese] [Carraro] [2068226]
+// [Giovanni Battista] [Matteazzi] [2082852]
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+public class IntegerToRoman {
+    public static String convert(int number){
+        // TODO
+        return null;
+    }
+}
---
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////////////////////////
+// [Agnese] [Carraro] [2068226]
+// [Giovanni Battista] [Matteazzi] [2082852]
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+public class RomanPrinter {
+    public static String print(int num){
+        return printAsciiArt(IntegerToRoman.convert(num));
+    }
+    private static String printAsciiArt(String romanNumber){
+        //TODO
+        return null;
+    }
+}
