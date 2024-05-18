1c1c558935d9d07b0a79aabdf3f36b0adb317188
gb-993 <gbmatteazzi93@gmail.com>
2024-05-18 17:08:28 +0200

    conversione e test conversione (1 -> 9)

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -6,8 +6,16 @@
 package it.unipd.mtss;
 
 public class IntegerToRoman {
-    public static String convert(int number){
-        // TODO
-        return null;
+    public static String convert(int number) {
+        String result = "";
+
+        String[] c1 = {"I","II","III","IV","V", "VI", "VII", "VIII", "IX"};
+
+        if (number > 0 && number < 10) {
+            result = result.concat(c1[number-1]);
+        }
+
+        if (!result.isEmpty()) {return result;}
+        else {return null;}
     }
 }
--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -4,6 +4,24 @@
 ////////////////////////////////////////////////////////////////////
 
 package it.unipd.mtss;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class IntegerToRomanTest {
+    @Test
+    public void NullTest() {
+        assertNull(IntegerToRoman.convert(-2));
+        assertNull(IntegerToRoman.convert(0));
+        assertNull(IntegerToRoman.convert(25));
+    }
+
+
+    @Test
+    public void NumbersTest() {
+        assertEquals("II", IntegerToRoman.convert(2));
+        assertEquals("V", IntegerToRoman.convert(5));
+        assertEquals("VIII", IntegerToRoman.convert(8));
+    }
+
 }
