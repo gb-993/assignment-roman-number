73b5a5c3e3be35d527c4b79b8a03980204c62e46
gb-993 <gbmatteazzi93@gmail.com>
2024-05-18 17:26:02 +0200

    stampa e test stampa (1 -> 9)

--- src/main/java/it/unipd/mtss/RomanPrinter.java
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -10,7 +10,38 @@ public class RomanPrinter {
         return printAsciiArt(IntegerToRoman.convert(num));
     }
     private static String printAsciiArt(String romanNumber){
-        //TODO
-        return null;
+        String result = "";
+
+        // lettera I
+        String[] i = {" _____ ", "|_   _|", "  | |  ",
+                "  | |  ", " _| |_ ", "|_____|"};
+
+        // lettera V
+        String[] v = {" __      __ ", " \\ \\    / / ",
+                "  \\ \\  / /  ", "   \\ \\/ /   ",
+                "    \\  /    ", "     \\/     "};
+
+        // lettera X
+        String[] x = {" __   __ ", " \\ \\ / / ", "  \\ V /  ",
+                "   > <   ", "  / . \\  ", " /_/ \\_\\ "};
+
+        if (romanNumber != null) {
+            for (int j = 0; j < 6; j++) {
+                for (int k = 0; k < romanNumber.length(); k++) {
+                    char letter = romanNumber.charAt(k);
+                    if (letter == 'I') {
+                        result = result.concat(i[j]);
+                    } else if (letter == 'V') {
+                        result = result.concat(v[j]);
+                    } else if (letter == 'X') {
+                        result = result.concat(x[j]);
+                    }
+                }
+                result = result.concat("\n");
+            }
+        }
+
+        if (!result.isEmpty()) {return result;}
+        else {return null;}
     }
 }
--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -1,9 +1,53 @@
-////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;////////////////////////////////////////////////////////////////////
 // [Agnese] [Carraro] [2068226]
 // [Giovanni Battista] [Matteazzi] [2082852]
 ////////////////////////////////////////////////////////////////////
 
-package it.unipd.mtss;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class RomanPrinterTest {
+    @Test
+    public void NullTest() {
+        assertNull(RomanPrinter.print(-8));
+        assertNull(RomanPrinter.print(0));
+        assertNull(RomanPrinter.print(19));
+    }
+    @Test
+    public void NumbersTest() {
+        assertEquals("""
+                 _____  _____  _____\s
+                |_   _||_   _||_   _|
+                  | |    | |    | | \s
+                  | |    | |    | | \s
+                 _| |_  _| |_  _| |_\s
+                |_____||_____||_____|
+                """, RomanPrinter.print(3));
+        assertEquals("""
+                 _____  __      __\s
+                |_   _| \\ \\    / /\s
+                  | |    \\ \\  / / \s
+                  | |     \\ \\/ /  \s
+                 _| |_     \\  /   \s
+                |_____|     \\/    \s
+                """, RomanPrinter.print(4));
+        assertEquals("""
+                 __      __  _____  _____\s
+                 \\ \\    / / |_   _||_   _|
+                  \\ \\  / /    | |    | | \s
+                   \\ \\/ /     | |    | | \s
+                    \\  /     _| |_  _| |_\s
+                     \\/     |_____||_____|
+                """, RomanPrinter.print(7));
+        assertEquals("""
+                 _____  __   __\s
+                |_   _| \\ \\ / /\s
+                  | |    \\ V / \s
+                  | |     > <  \s
+                 _| |_   / . \\ \s
+                |_____| /_/ \\_\\\s
+                """, RomanPrinter.print(9));
+    }
 }
